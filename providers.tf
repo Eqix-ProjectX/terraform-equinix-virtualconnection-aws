bf486f1ac2f703eb681adfbd19481d42852e829c
workworkworkworkwork247365 <149482238+workworkworkworkwork247365@users.noreply.github.com>
2024-10-10 03:41:14 +0800

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -5,10 +5,21 @@ terraform {
       source = "equinix/equinix"
       #version = "1.36.4"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"  
+    }
+
   }
 }
 
 provider equinix {
   client_id     = var.equinix_client_id
   client_secret = var.equinix_client_secret
+}
+
+provider "aws" {
+  region = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
