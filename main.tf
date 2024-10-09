9e84a94c8b4e62fb2df219ab6e668905ec2b2445
workworkworkworkwork247365 <149482238+workworkworkworkwork247365@users.noreply.github.com>
2024-10-10 03:35:36 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
-
+resource "random_pet" "this" {
+  length = 2
+}
 
 resource "equinix_fabric_connection" "vd2aws" {
-name = var.connection_name
+  name = "${var.connection_name}${random_pet.this.id }"
   type = var.connection_type
   notifications {
     type   = "ALL"
@@ -41,3 +43,60 @@ name = var.connection_name
   }
 }
 
+locals {
+    provider_connection_ids = [
+    for access_point in [
+      for z in equinix_fabric_connection.vd2aws.z_side : tolist(z.access_point)[0]
+    ] : access_point.provider_connection_id
+  ]  
+}
+
+resource "aws_dx_connection_confirmation" "aws_dx_connection_confirm" {
+  connection_id = local.provider_connection_ids[0]
+  
+}
+
+data "aws_dx_connection" "dx_connection" {
+  depends_on = [ equinix_fabric_connection.vd2aws ]
+  name = "${var.connection_name}${random_pet.this.id }"
+}
+
+
+resource "aws_vpc" "main" {
+  cidr_block = var.aws_vpc_cidr
+  tags = {
+    Name = var.aws_vpc_name
+  }
+}
+
+resource "aws_subnet" "private" {
+  depends_on = [aws_vpc.main ]
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.aws_subnet_cidr
+   tags = {
+    Name = var.aws_subnet_name
+  }
+}
+
+resource "aws_vpn_gateway" "vgw" {
+   depends_on = [aws_subnet.private ]
+  vpc_id = aws_vpc.main.id
+tags = {
+    Name = var.aws_vpg_name
+  }
+  
+}
+
+resource "aws_dx_private_virtual_interface" "aws_dx_vif" {
+  depends_on = [aws_dx_connection_confirmation.aws_dx_connection_confirm ]
+  connection_id    = local.provider_connection_ids[0] 
+  name             = var.aws_vif_name
+  vlan             = data.aws_dx_connection.dx_connection.vlan_id
+  address_family   = "ipv4"
+  bgp_asn          = var.customer_asn
+  bgp_auth_key     = var.aws_bgp_auth_key
+  amazon_address   = var.amazon_ip_address
+  customer_address = var.customer_ip_address
+  mtu              = 1500
+  vpn_gateway_id   = aws_vpn_gateway.vgw.id
+} 
