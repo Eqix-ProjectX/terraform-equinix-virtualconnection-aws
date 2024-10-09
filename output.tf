16995e079b277d2b2e49d5fcdc5f3e0949ca9efd
workworkworkworkwork247365 <149482238+workworkworkworkwork247365@users.noreply.github.com>
2024-10-10 03:44:58 +0800

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,19 @@
 output "AWS_VC_id" {
   value = equinix_fabric_connection.vd2aws.id
 }
+
+output "AWS_DX_id" {
+value = data.aws_dx_connection.dx_connection.id
+}
+
+output "AWS_VPG_id" {
+  value = aws_vpn_gateway.vgw.id
+}
+
+output "AWS_VPC_id" {
+  value = aws_vpc.main.id
+}
+
+output "AWS_Subnet_id" {
+  value = aws_subnet.private.id
+}
